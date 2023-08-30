da7fc043465ba3ca2b6a7f6e15a9285bc38cf7d9
Anibal <anibalmreinoso@gmail.com>
2023-08-30 20:17:22 -0300

    templates, y cambios en views y urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('posteos/', views.posteos, name='posteos'),
+    path('crear_posteo/', views.crear_posteo, name='crear_posteo'),
+    path('modificar_posteo/<id>/', views.modificar_posteo, name='modificar_posteo'),
+    path('eliminar_posteo/<id>/', views.eliminar_posteo, name='eliminar_posteo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,74 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
-    content = HttpResponse('<div><h1>Nuestra primera aplicación!</h1></div>')
-    data = {"name": "Juanito", "edad": 10}
-
-    context = {'content': content,
-               'data': data}
-    
-    return render(request=request, template_name='index.html', context={'context':context})
+    return HttpResponse('<div><h1>Nuestra primera aplicación!</h1></div>')
 
 def posteos(request):
     """
     Funcion para mostrar posteos de la BD
     """
     filtro = request.GET.get('filtro')
+    segundo_filtro = request.GET.get('segundo_filtro')
 
     if filtro:
         posts = Post.objects.filter(title__contains=filtro)
     else:
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-author')
+    
+    if segundo_filtro:
+        posts = Post.objects.filter(author=segundo_filtro)
 
     context = {'posteos': posts}
     return render(request, 'blog/posteos.html', {"context":context})
 
+def crear_posteo(request):
+    """
+    Formulario para creación de posteos
+    """
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            author = form.cleaned_data["author"]
+            tzone = form.cleaned_data["tzone"]
+            post = Post(
+                title=title,
+                content=content,
+                author=author,
+                tzone=tzone
+            )
+            post.save()
+            return redirect('posteos')
+        else:
+            return render(request, 'blog/crear_posteo.html', {"form":form})
 
+    return render(request, 'blog/crear_posteo.html', {"form":form})
+
+
+def modificar_posteo(request, id):
+    """
+    Funcion para modificar posteos por id
+    """
+    post = Post.objects.get(pk=id)
+    form = PostForm(instance=post)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        form.save()
+        return redirect('posteos')
+    
+    return render(request, 'blog/modificar_posteo.html', {"form": form})
+
+def eliminar_posteo(request, id):
+    post = Post.objects.get(pk=id)
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, 'Se elimino exitosamente!')
+        return redirect('posteos')
