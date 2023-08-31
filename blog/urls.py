f939b2148a7a51a2cb4cd9f9d6fd5babd340243f
Anibal <anibalmreinoso@gmail.com>
2023-08-30 21:55:53 -0300

    class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('posteos/', views.posteos, name='posteos'),
-    path('crear_posteo/', views.crear_posteo, name='crear_posteo'),
-    path('modificar_posteo/<id>/', views.modificar_posteo, name='modificar_posteo'),
-    path('eliminar_posteo/<id>/', views.eliminar_posteo, name='eliminar_posteo'),
+    # path('posteos/', views.posteos, name='posteos'),
+    path('posteos/', PostListView.as_view(), name='posteos'),
+    # path('crear_posteo/', views.crear_posteo, name='crear_posteo'),
+    path('crear_posteo/', PostCreateView.as_view(), name='crear_posteo'),
+    # path('modificar_posteo/<id>/', views.modificar_posteo, name='modificar_posteo'),
+    path('modificar_posteo/<pk>/', PostUpdateView.as_view(), name='modificar_posteo'),
+    # path('eliminar_posteo/<id>/', views.eliminar_posteo, name='eliminar_posteo'),
+    path('eliminar_posteo/<pk>/', PostDeleteView.as_view(), name='eliminar_posteo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,74 +1,110 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('<div><h1>Nuestra primera aplicación!</h1></div>')
 
-def posteos(request):
-    """
-    Funcion para mostrar posteos de la BD
-    """
-    filtro = request.GET.get('filtro')
-    segundo_filtro = request.GET.get('segundo_filtro')
+# def posteos(request):
+#     """
+#     Funcion para mostrar posteos de la BD
+#     """
+#     filtro = request.GET.get('filtro')
+#     segundo_filtro = request.GET.get('segundo_filtro')
 
-    if filtro:
-        posts = Post.objects.filter(title__contains=filtro)
-    else:
-        posts = Post.objects.all().order_by('-author')
+#     if filtro:
+#         posts = Post.objects.filter(title__contains=filtro)
+#     else:
+#         posts = Post.objects.all().order_by('-author')
     
-    if segundo_filtro:
-        posts = Post.objects.filter(author=segundo_filtro)
+#     if segundo_filtro:
+#         posts = Post.objects.filter(author=segundo_filtro)
 
-    context = {'posteos': posts}
-    return render(request, 'blog/posteos.html', {"context":context})
+#     context = {'posteos': posts}
+#     return render(request, 'blog/posteos.html', {"context":context})
 
-def crear_posteo(request):
-    """
-    Formulario para creación de posteos
-    """
-    form = PostForm()
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data["title"]
-            content = form.cleaned_data["content"]
-            author = form.cleaned_data["author"]
-            tzone = form.cleaned_data["tzone"]
-            post = Post(
-                title=title,
-                content=content,
-                author=author,
-                tzone=tzone
-            )
-            post.save()
-            return redirect('posteos')
-        else:
-            return render(request, 'blog/crear_posteo.html', {"form":form})
+# def crear_posteo(request):
+#     """
+#     Formulario para creación de posteos
+#     """
+#     form = PostForm()
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             title = form.cleaned_data["title"]
+#             content = form.cleaned_data["content"]
+#             author = form.cleaned_data["author"]
+#             tzone = form.cleaned_data["tzone"]
+#             post = Post(
+#                 title=title,
+#                 content=content,
+#                 author=author,
+#                 tzone=tzone
+#             )
+#             post.save()
+#             return redirect('posteos')
+#         else:
+#             return render(request, 'blog/crear_posteo.html', {"form":form})
 
-    return render(request, 'blog/crear_posteo.html', {"form":form})
+#     return render(request, 'blog/crear_posteo.html', {"form":form})
 
 
-def modificar_posteo(request, id):
-    """
-    Funcion para modificar posteos por id
-    """
-    post = Post.objects.get(pk=id)
-    form = PostForm(instance=post)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        form.save()
-        return redirect('posteos')
+# def modificar_posteo(request, id):
+#     """
+#     Funcion para modificar posteos por id
+#     """
+#     post = Post.objects.get(pk=id)
+#     form = PostForm(instance=post)
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, instance=post)
+#         form.save()
+#         return redirect('posteos')
     
-    return render(request, 'blog/modificar_posteo.html', {"form": form})
+#     return render(request, 'blog/modificar_posteo.html', {"form": form})
 
 def eliminar_posteo(request, id):
+    """
+    Funcion para eliminar posteos por id
+    """
     post = Post.objects.get(pk=id)
     if request.method == 'POST':
         post.delete()
         messages.success(request, 'Se elimino exitosamente!')
         return redirect('posteos')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/posteos.html'
+    context_object_name = 'context'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro = self.request.GET.get('filtro')
+
+        if filtro:
+            queryset = queryset.filter(title__contains=filtro)
+        
+        return queryset
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/crear_posteo.html'
+    success_url = reverse_lazy('posteos')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/modificar_posteo.html'
+    success_url = reverse_lazy('posteos')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posteos')
